b3b7e6d89638f1ee5118856c216d9614beb09cd7
HristoSLV <hristoivanovslavchev@gmail.com>
2025-08-12 18:39:43 +0300

    initial commit

---
+++ src/main/java/com/sirmascademy/SirmaRetakeExam/model/ActorPair.java
@@ -0,0 +1,16 @@
+package com.sirmascademy.SirmaRetakeExam.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActorPair {
+
+    private ActorEntity actorOne;
+
+    private ActorEntity actorTwo;
+
+}
